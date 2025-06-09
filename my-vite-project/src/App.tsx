a60199db83eb8abc9763ea6f65c45a877cf68cd2
adamglowienke03 <glowienke_adam@wp.pl>
2025-06-10 00:15:40 +0200

    miniUpdate

--- my-vite-project/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-
-function App() {
-  console.log("Lorem ipsum!")
-  return <h1>Hello World!</h1>
-}
-
-export default App
--- my-vite-project/src/main.tsx
+++ my-vite-project/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+const value1 = Number(prompt("Podaj pierwszą liczbę: "))
+const value2 = Number(prompt("Podaj drugą liczbę: "))
+
+const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const myName = "Adam";
+
+const transformMe = (value: string) => {
+  return value.toLocaleUpperCase();
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <h1>Hello {transformMe(myName)}, {value1 + value2}, {values.filter(el => el % 2).map(el => el ** 2).join("-")}</h1>
 )
